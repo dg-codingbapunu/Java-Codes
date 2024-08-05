4186a6cb2143e15509a521d4728607d8ac2ee330
patitapaban <patitapabanpanda046@gmail.com>
2024-08-05 14:50:06 +0530

    Type Promotion in java

--- hello.java
+++ hello.java
@@ -1,14 +1,18 @@
 class Hello {
 
 public static void main(String[] args) {
-int a = 390;
-byte b = 127;
-b = (byte)a;
-float f = 6.9f;
-int t = (int)f;
-System.out.println(t);
-System.out.println(b);
-
+// int a = 390;
+// byte b = 127;
+// b = (byte)a;
+// float f = 6.9f;
+// int t = (int)f;
+// System.out.println(t);
+// System.out.println(b);
+byte a = 20;
+byte b = 40;
+int result = a * b;
+System.out.println(result);
+// byte max value 127
 
 }
 
