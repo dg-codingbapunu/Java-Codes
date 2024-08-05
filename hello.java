e17641424037886fb9c94a8fd8da5728e39242f0
patitapaban <patitapabanpanda046@gmail.com>
2024-08-05 11:55:04 +0530

    datatypes

---
+++ hello.java
@@ -0,0 +1,18 @@
+class Hello {
+
+public static void main(String[] args) {
+int num1 = 9;
+byte by = 126;
+short sh = 558;
+long l = 23748l;
+float f = 5.89f;
+double d = 5.89;
+char c = 'B';
+boolean b = false;
+
+
+}
+
+
+
+}
