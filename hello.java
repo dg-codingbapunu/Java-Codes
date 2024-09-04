f4657acb26b0f82fb1e89c9470875c48267d6e20
patitapaban <patitapabanpanda046@gmail.com>
2024-09-04 10:38:00 +0530

    object and class theory

--- hello.java
+++ hello.java
@@ -1,14 +1,19 @@
-class Hello {
-  public static void main(String[] args) 
-{
 
-
-for (int i = 4; i >=1; i--) {
-  System.out.println("Hello " + i);
+ class calculater {
+  int a;
+  public int add(int n1, int n2) {
+    return n1+n2;
+  }
 }
 
 
+  class hello {
+  public static void main(String[] args) {
+int num1 = 4;
+int num2 = 5;
 
+    calculater calc = new calculater();
+    int result = calc.add( num1,  num2);
+    System.out.println(result);
+  }
 }
-
-           }
