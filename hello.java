80ee669cf4497d9b9a962384b09e466735579dac
patitapaban <patitapabanpanda046@gmail.com>
2024-09-09 11:52:05 +0530

    student table

--- hello.java
+++ hello.java
@@ -1,23 +1,36 @@
+
+class Student {
+  int rollno;
+  String name;
+  int marks;
+}
+
+
+
+
 public class Hello {
 
 public static void main(String[] args) {
+  Student s1 = new Student();
+s1.rollno = 1;
+s1.name = "Bapunu";
+s1.marks = 90;
 
-  int nums[][] = new int[3][4];
-  for(int i = 0; i < 3; i++) {
-    for (int j = 0; j < 4; j++) {
-     nums[i][j] = (int)(Math.random() * 10);
-    }
-   
+Student s2 = new Student();
+s2.rollno = 2;
+s2.name = "nandu";
+s2.marks = 80;
+
+Student s3 = new Student();
+s3.rollno = 2;
+s3.name = "sudhia";
+s3.marks = 50;
+Student students[] = new Student[3];
+students[0] = s1;
+students[1] = s2;
+students[2] = s3;
+  for (int i = 0; i < students.length ; i ++) {
+    System.out.println(students[i].name + " : " + students[i].marks);
   }
-
-
-
-  for(int i = 0; i < 3; i++) {
-    for (int j = 0; j < 4; j++) {
-      System.out.print(nums[i][j] + " ");
-    }
-    System.out.println();
-  }
-  
 }
 }
