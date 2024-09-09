ed219a77f441fa22381458c3fcb657e6c2db042d
patitapaban <patitapabanpanda046@gmail.com>
2024-09-09 10:21:31 +0530

    2 dimensoinal array

--- hello.java
+++ hello.java
@@ -1,12 +1,23 @@
-public class Hello 
-{
-  public static void main(String[] args) 
-  {
+public class Hello {
 
-    int nums[] = {1, 2, 3, 4, 5 ,6};
-   for (int i = 0; i < 6; i++) {
-    System.out.println(nums[i]);
-   }
-    
+public static void main(String[] args) {
+
+  int nums[][] = new int[3][4];
+  for(int i = 0; i < 3; i++) {
+    for (int j = 0; j < 4; j++) {
+     nums[i][j] = (int)(Math.random() * 10);
+    }
+   
   }
+
+
+
+  for(int i = 0; i < 3; i++) {
+    for (int j = 0; j < 4; j++) {
+      System.out.print(nums[i][j] + " ");
+    }
+    System.out.println();
+  }
+  
+}
 }
