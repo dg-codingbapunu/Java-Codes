c384e4e9bb1eebc840ee235c5399a7c25f22e5a0
patitapaban <patitapabanpanda046@gmail.com>
2024-09-11 11:48:32 +0530

    use of static key word

--- hello.java
+++ hello.java
@@ -1,7 +1,34 @@
+class Mobile {
+    String brands;
+ int price;
+ static String name;
+
+  public void show() {
+    System.out.println(brands + " : " + price + " : " + name);
+  }
+  
+
+}
+
+
+
 public class Hello {
   public static void main(String[] args) {
-   StringBuffer sb = new StringBuffer("bapunu sir");
+Mobile obj1 = new Mobile();
+obj1.brands = "Apple";
+Mobile.name = "Hand phone";
+obj1.price = 100000;
+
+Mobile obj2 = new Mobile();
+obj2.brands = "oppo";
+Mobile.name = "Smart Phone";
+obj2.price = 150000;
+
+Mobile.name = "phone";
+
+
+obj1.show();
+obj2.show();
   
- System.out.println(sb.insert(10, "java lover"));
   }
 }
