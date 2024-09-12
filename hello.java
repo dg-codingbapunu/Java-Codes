765782798130d292c46c620e42b56de664b67a59
patitapaban <patitapabanpanda046@gmail.com>
2024-09-12 09:51:13 +0530

    this key word in java

--- hello.java
+++ hello.java
@@ -3,47 +3,40 @@ class Human
    private int age;
    private String name;
    private int rollNo;
+  public int getAge() {
+    return age;
+  }
+  public void setAge(int age) {
+    this.age = age;
+  }
+  public String getName() {
+    return name;
+  }
+  public void setName(String name) {
+    this.name = name;
+  }
+  public int getRollNo() {
+    return rollNo;
+  }
+  public void setRollNo(int rollNo) {
+    this.rollNo = rollNo;
+  }
 
-public int getAge()
-{
-  return age;
-}
-public void setAge(int a)
-{
-  age = a;
-}
 
-public String getName()
-{
-  return name;
-}
-public void setName(String n) 
-{
-  name = n;
-
-}
-public int getRoll()
-{
-  return rollNo;
-}
-
-public void setRoll(int r) 
-
-{
-rollNo = r;
-}
 
 }
 
 
 public class Hello 
 {
-  public static void main(String[] args) {
+  public static void main(String[] args) 
+  {
+
 Human obj = new Human();
 obj.setAge(22);
 obj.setName("bapunu");
-obj.setRoll(143);
-System.out.println(obj.getName() + " : " + obj.getAge() + " : " + obj.getRoll());
+obj.setRollNo(143);
+System.out.println(obj.getName() + " : " + obj.getAge() + " : " + obj.getRollNo());
 
     
   }
