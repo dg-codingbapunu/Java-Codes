0393af9a60202ab73395d15c95fb13ff0a52f564
patitapaban <patitapabanpanda046@gmail.com>
2024-09-12 10:40:53 +0530

    constructor in java

--- hello.java
+++ hello.java
@@ -3,6 +3,19 @@ class Human
    private int age;
    private String name;
    private int rollNo;
+
+public Human()   // default constructer
+{
+ age = 22;
+ name = "bapunu";
+ rollNo = 34;
+}
+public Human(int a, String n , int r) // parameterized constructor
+{
+name = n;
+age = a;
+rollNo = r;
+}
   public int getAge() {
     return age;
   }
@@ -32,12 +45,16 @@ public class Hello
   public static void main(String[] args) 
   {
 
-Human obj = new Human();
-obj.setAge(22);
-obj.setName("bapunu");
-obj.setRollNo(143);
-System.out.println(obj.getName() + " : " + obj.getAge() + " : " + obj.getRollNo());
+Human obj = new Human(16 , "bapunu", 143);
 
-    
-  }
+
+System.out.println(obj.getName() + " : " + obj.getAge() + " : " + obj.getRollNo());
+// obj.setAge(22);
+// obj.setName("bapunu");
+// obj.setRollNo(143);
+// System.out.println(obj.getName() + " : " + obj.getAge() + " : " + obj.getRollNo());
+
+ 
+}
+
 }
