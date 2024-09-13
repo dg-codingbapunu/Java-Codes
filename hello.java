d690e38ad0d99a23ab878bc7b8b85dec460d6c89
patitapaban <patitapabanpanda046@gmail.com>
2024-09-13 12:25:40 +0530

    Inheritance injava

--- hello.java
+++ hello.java
@@ -1,23 +1,19 @@
 
-class A 
-{
-  public void show()
-  {
-    System.out.println("in show method");
-  }
-  public A()
-  {
-    System.out.println("object created");
-  }
-}
 
 
-class Hello
+
+class Hello 
 {
   public static void main(String[] args) 
+  
   {
-    new A(); // anonymous object
-  new A().show();
-   
+    VeryAdvCalc obj = new VeryAdvCalc();
+    int r1 = obj.add(239, 6450);
+    int r2 = obj.sub(456, 78);
+    int r3 = obj.multy(23, 89);
+    int r4 = obj.div(45, 9);
+    double r5 = obj.power(2, 12);
+
+    System.out.println(r1 + "  : " + r2 + " : " + r3 + " :  " + r4 + " : " + r5);
   }
 }
