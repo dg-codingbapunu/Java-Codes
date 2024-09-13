6d536352ea7f89738108936f76adac02edb1b084
patitapaban <patitapabanpanda046@gmail.com>
2024-09-13 10:40:31 +0530

    anonymous object in java

--- hello.java
+++ hello.java
@@ -1,38 +1,23 @@
+
 class A 
 {
-public A() 
-{
-  super();
-System.out.println("in A");
-}
-public A(int n)
-{
-  super();
-System.out.println("int A");
-}
-}
-
-class B extends A 
-{
-public B()
-{
-  super();
-  System.out.println("in B");
-}
-public B(int a)
-{
-  this();
-  System.out.println("int B ");
-}
+  public void show()
+  {
+    System.out.println("in show method");
+  }
+  public A()
+  {
+    System.out.println("object created");
+  }
 }
 
 
-
-public class Hello {
-public static void main(String[] args) 
+class Hello
 {
-  B obj = new B(5);
-  
-}
-  
+  public static void main(String[] args) 
+  {
+    new A(); // anonymous object
+  new A().show();
+   
+  }
 }
