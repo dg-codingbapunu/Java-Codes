27dab51051acd144288695d44bf36422b96eff81
patitapaban <patitapabanpanda046@gmail.com>
2024-09-16 09:10:25 +0530

    final-variable

--- hello.java
+++ hello.java
@@ -1,41 +1,10 @@
-class A 
-{
-  public void show()
-{
-  System.out.println("in A show");
-}
-
-}
-class B extends A
-{
-public void show()
-{
-  System.out.println("in B show");
-}
-}
-class C extends A
-{
-
-public void show()
-{
-  System.out.println("in C show");
-}
-
-}
-
-public class Hello
+class Hello
 {
   public static void main(String[] args) 
   {
-    A obj = new A();
-    obj.show();
-
-    obj = new B();
-    obj.show();
-    
-    obj = new C();
-    obj.show();
+    final int num = 8;
+    // num = 9; cant change value once assigned final
+    System.out.println(num);
     
   }
-
 }
