9e79eb2043902def6ec8aaf2a2ea7a79b4c0800d
patitapaban <patitapabanpanda046@gmail.com>
2024-09-16 09:23:45 +0530

    final- class

--- hello.java
+++ hello.java
@@ -1,10 +1,30 @@
+ final class  Calc 
+{
+  public void show()
+{
+  System.out.println("in Bapun show");
+}
+public void add(int n1, int n2)
+{
+System.out.println(n1 + n2);
+}
+
+}
+
+class AdvCalc extends Calc // make finnal class to stop inheritance of ypur class
+{
+
+}
+
+
+
 class Hello
 {
   public static void main(String[] args) 
   {
-    final int num = 8;
-    // num = 9; cant change value once assigned final
-    System.out.println(num);
-    
+Calc obj = new Calc();
+obj.show();
+obj.add(3, 8);
+   
   }
 }
