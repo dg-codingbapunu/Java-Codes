3e2104b6db186c341afbfaf953b601a886a77855
patitapaban <patitapabanpanda046@gmail.com>
2024-09-16 10:18:38 +0530

    type casting

--- hello.java
+++ hello.java
@@ -1,44 +1,10 @@
-class Laptop
-{
-String model;
-int price;
-
-public String toString()
-{
-  return model + " : " + price;
-
-}
-
-public boolean equals(Laptop that)
-{
-if(this.model.equals(that.model) && this.price == that.price)
-{
-  return true;
-}
-else 
-{
-  return false;
-}
-}
-
-}
-
-
-
-class Hello 
+public class Hello
 {
   public static void main(String[] args) 
   {
-    Laptop obj = new Laptop();
-    obj.model = "lenove 90";
-    obj.price = 40000;
 
-    Laptop obj1 = new Laptop();
-    obj1.model = "lenove 90";
-    obj1.price = 40000;
-
-    boolean result = obj.equals(obj1);
-
-    System.out.println(result);
+    double d = 8.9;
+    int i = (int) d;
+    System.out.println(i); // type casting
   }
 }
