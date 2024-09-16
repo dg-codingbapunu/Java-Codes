4f26ffe64e6a02347d63d097bf2c32c9fd1e4607
patitapaban <patitapabanpanda046@gmail.com>
2024-09-16 10:34:24 +0530

    upcasting & downcasting

--- hello.java
+++ hello.java
@@ -1,10 +1,27 @@
+class A 
+{
+  public void show1()
+  {
+System.out.println("in A show");
+  }
+}
+class B extends A
+{
+  public void show2()
+  {
+    System.out.println("in B show");
+  }
+}
+
 public class Hello
 {
   public static void main(String[] args) 
   {
+A obj = (A) new B(); // upcasting
+obj.show1(); 
+ B obj1 =(B) obj; // downcasting
+ obj1.show2();
 
-    double d = 8.9;
-    int i = (int) d;
-    System.out.println(i); // type casting
+   
   }
 }
