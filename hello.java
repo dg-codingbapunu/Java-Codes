861f6fb0f4de2f526c53d999bd712bbe6e0e50f3
patitapaban <patitapabanpanda046@gmail.com>
2024-09-17 10:28:37 +0530

    anonymous inner class

--- hello.java
+++ hello.java
@@ -2,29 +2,25 @@ class A
 {
   public void show()
   {
-    System.out.println("In A show");
+    System.out.println("in A show");
   }
+}
 
-class B 
+
+
+class Hello
 {
-  public void config()
-  {
-    System.out.println("in B show");
-  }
-}
-
-}
-
-
-
-public class Hello {
-
   public static void main(String[] args) 
   {
-    A obj = new A();
-    obj.show();
-  A.B obj1 = obj.new B();
-  obj1.config();
+    A obj = new A()
+    {
+      public void show()
+      {
+        System.out.println("in new show");
+      }
+    }
     
+    ;
+    obj.show();
   }
 }
