ec339b483e26f40ff2c5c96f614e9e159618c95e
patitapaban <patitapabanpanda046@gmail.com>
2024-09-18 09:40:28 +0530

    if/else with enum

--- hello.java
+++ hello.java
@@ -13,13 +13,21 @@ public class Hello
   public static void main(String[] args) 
   
   {
-    Status [] ss = Status.values();
-    
    
-   
-   for(Status s : ss)
-   {
-    System.out.println(s);
-   }
+    Status s = Status.Success;
+    if(s == Status.Failed)
+    {
+      System.out.println("Try Again");
+    } else if(s == Status.Pending)
+    {
+      System.out.println("please Wait");
+    } else if (s == Status.Running) 
+    {
+      System.out.println("All Good");
+    } else 
+      {
+System.out.println("Done");
+
+      }
   }
 }
