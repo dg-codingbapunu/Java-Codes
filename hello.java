592d29aec8aa038e82df6509347198b1575786ff
patitapaban <patitapabanpanda046@gmail.com>
2024-09-22 10:30:50 +0530

    Comparator

--- hello.java
+++ hello.java
@@ -1,19 +1,52 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 
 public class Hello 
 {
 
   public static void main(String[] args) 
   {
-    Map<String, Integer> students = new HashMap<>();
 
-    students.put("bapunu", 62);
-    students.put("sinu", 7);
-    students.put("Harsh", 60);
-    students.put("ladu", 92);
-    students.put("ladu", 100);// key are unique
+    Comparator<Integer> comp = new Comparator<Integer>() 
+    
+    {
 
+      public int compare(Integer i, Integer j)
+     {
+     
+        if(i%10 > j%10)
+        {
+          return 1;
+        }
+
+         
+      else {
+        return -1;
+      }
+       
+      
+      
+     }
+      
+    };
+
+
+
+
+List<Integer> nums = new ArrayList<>();
+
+
+nums.add(23);
+nums.add(65);
+nums.add(71);
+nums.add(79);      // samall to big number sorting 
+
+Collections.sort(nums,comp);
+
+System.out.println(nums);
   
   }
   
