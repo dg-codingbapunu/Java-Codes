04d1a021a23c40f2d2008992fda4bd5d9883577a
patitapaban <patitapabanpanda046@gmail.com>
2024-09-24 12:04:19 +0530

    Reverse an array

--- hello.java
+++ hello.java
@@ -1,32 +1,22 @@
-import java.util.ArrayList;
-import java.util.List;
+// reverse an array
 
+public class Hello {
 
-public class Hello
-{
-  public static void main(String[] args) 
-  {
-
-  List <Integer> nums = new ArrayList<>();
-
-  
-  nums.add(8);
-  nums.add(18);
-  nums.add(67);
-  nums.add(12);
-  nums.add(6);
-  nums.add(79);
-
-
-int result = nums.stream()
-              .filter(i -> i%2==0)
-              .map(i -> i *2)
-              .reduce(0,(p,e) -> p + e);
-
-System.out.println(result);
-
-// you cant print s1 (stream) then one time it gives us copilation error
-
+  public static void main(String[] args) {
     
+int arr [] = new int[]{1,2,3,4,5};
+int i;
+
+for (i = 0;  i < arr.length; i++)
+{
+  System.out.print(arr[i]);
+}
+
+
+for (i = arr.length -1; i >=0; i--)
+{
+  System.out.print(arr[i] + " ");
+}
+System.out.println();
   }
 }
