ee9fe943b4b0e9d99bea6219d61ba18c7bfa5638
patitapaban <patitapabanpanda046@gmail.com>
2024-10-05 09:29:37 +0530

    triangle

--- hello.java
+++ hello.java
@@ -4,19 +4,46 @@ public class Hello {
 
   public static void main(String[] args) {
     
-int arr [] = new int[]{1,2,3,4,5};
-int i;
-
-for (i = 0;  i < arr.length; i++)
-{
-  System.out.print(arr[i]);
-}
+int n = 7;
 
 
-for (i = arr.length -1; i >=0; i--)
-{
-  System.out.print(arr[i] + " ");
-}
-System.out.println();
+    for (int i = 0; i < n; i++){
+
+      StringBuilder row = new StringBuilder();
+      
+      
+      
+      // space
+      
+      for (int j = 0; j < n-i-1; j++){
+          row.append(" ");
+      }
+      for (int j = 0; j < 2*i+1; j++){
+          row.append("*");
+      }
+      System.out.println(row);
+      
+      }
+      
+      // 2nd pattern
+      for (int i = 0; i < n; i++){
+      
+      StringBuilder row = new StringBuilder();
+      
+      
+      
+      // space
+      
+      for (int j = 0; j < i; j++){
+          row.append(" ");
+      }
+      for (int j = 0; j <2*n- (2*i+1); j++){
+          row.append("*");
+      }
+      System.out.println(row);
+      
+      }
+      
+
   }
 }
