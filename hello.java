ef1a094493e012b92bccee923a6e454b4834ed05
patitapaban <patitapabanpanda046@gmail.com>
2024-11-06 10:38:18 +0530

    class & object

--- hello.java
+++ hello.java
@@ -1,49 +1,18 @@
-// reverse an array
-
 public class Hello {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
+        
+
+        Person p1 = new Person();
+        p1.age = 90;
+        p1.name = "bapunu";
+        System.out.println(p1.name + "" + p1.age);
+
+    }
+}
+
+class Person {
+    String name;
+    int age;
     
-int n = 7;
-
-
-    for (int i = 0; i < n; i++){
-
-      StringBuilder row = new StringBuilder();
-      
-      
-      
-      // space
-      
-      for (int j = 0; j < n-i-1; j++){
-          row.append(" ");
-      }
-      for (int j = 0; j < 2*i+1; j++){
-          row.append("*");
-      }
-      System.out.println(row);
-      
-      }
-      
-      // 2nd pattern
-      for (int i = 0; i < n; i++){
-      
-      StringBuilder row = new StringBuilder();
-      
-      
-      
-      // space
-      
-      for (int j = 0; j < i; j++){
-          row.append(" ");
-      }
-      for (int j = 0; j <2*n- (2*i+1); j++){
-          row.append("*");
-      }
-      System.out.println(row);
-      
-      }
-      
-
-  }
 }
