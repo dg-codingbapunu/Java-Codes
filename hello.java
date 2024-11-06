5ecb414a2723273846152464474fce1d2619151f
patitapaban <patitapabanpanda046@gmail.com>
2024-11-06 10:39:00 +0530

    methods

--- hello.java
+++ hello.java
@@ -8,6 +8,12 @@ public class Hello {
         p1.name = "bapunu";
         System.out.println(p1.name + "" + p1.age);
 
+        Person p2 = new Person();
+        p2.age = 80;
+        p2.name = "patitapabana";
+        System.out.println(p2.age + " " + p2.name);
+
+
     }
 }
 
