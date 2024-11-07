1545a9e63b12348e937af161c2bd5a36d497502c
patitapaban <patitapabanpanda046@gmail.com>
2024-11-07 12:44:27 +0530

    inter faces

--- hello.java
+++ hello.java
@@ -1,31 +1,16 @@
-public class Hello {
+public class Hello implements Car{
 
     public static void main(String[] args) {
-        Audi a1 = new Audi();
-        a1.start();
-        a1.show();
+        
+        
     }
-}
 
-class Bmw extends Car {
     @Override
-    void start() {
-        System.out.println("Bmw start");
+    public void start() {
+       System.out.println("inside interfaces");
     }
 }
 
-class Audi extends Car {
-    @Override
-    void start() {
-        System.out.println("audi started");
-
+    interface Car {
+        void start ();
     }
-    void show() {
-        System.out.println("hello");
-    }
-}
-
-abstract class Car {
-    int price;
-    abstract void start();
-}
