3d1e64a976895daad7d7059767c74d3736238277
patitapaban <patitapabanpanda046@gmail.com>
2024-11-08 11:51:50 +0530

    remove array

--- hello.java
+++ hello.java
@@ -10,11 +10,13 @@ public class Hello {
        list.add(1);
        list.add(9);
        list.add(7);
-       System.out.println(list);
+    //    System.out.println(list);
        
 list.add(1, 70);
 System.out.println(list);
-      
+// System.out.println(list.get(0));
+   list.remove(2);
+   System.out.println(list);
 
     }
 }
