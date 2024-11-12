4a97b68ea3b9b1d4409025dd5c02e68a25348d0a
patitapaban <patitapabanpanda046@gmail.com>
2024-11-12 11:16:38 +0530

    queue

--- hello.java
+++ hello.java
@@ -1,26 +1,24 @@
-import java.util.Stack;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Hello {
 
     public static void main(String[] args) {
         
-      
-     Stack <String> animal = new Stack<>();
-     animal.push("lion");  
-     animal.push("horse");   
-     animal.push("tiger");   
-     animal.push("elephant");   
- 
-    
-// System.out.println("stack:" +  animal);
+   
+        Queue <Integer> queue = new LinkedList<>();
 
-System.out.println(animal.peek());
-animal.pop();
-animal.pop();
 
-System.out.println(animal.peek());
+        queue.offer(12);
+        queue.offer(122);
+        queue.offer(123);
+        queue.offer(132);
 
-System.out.println("stack:" +  animal);
+        System.out.println("queue:" + queue);
+
+        System.out.println(queue.poll());
+        System.out.println("queue:" + queue);
+
 
     }
 }
