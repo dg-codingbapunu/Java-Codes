027cc002579b964319e152a1b95e82ada7740d87
patitapaban <patitapabanpanda046@gmail.com>
2024-11-12 12:13:16 +0530

    Hash set

--- hello.java
+++ hello.java
@@ -1,33 +1,27 @@
-import java.util.ArrayDeque;
-import java.util.Comparator;
-import java.util.LinkedList;
-import java.util.PriorityQueue;
-import java.util.Queue;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Hello {
 
     public static void main(String[] args) {
         
-   
-        ArrayDeque <Integer> adq = new ArrayDeque<>();
-        
-        adq.offer(34);
-        adq.offerFirst(2);
-        adq.offerLast(39);
-        adq.offerFirst(3);
-        System.out.println(adq);
-
-        System.out.println(adq.peek());
-        System.out.println(adq.peekFirst());
-        System.out.println(adq.peekLast());
+   // set not allowed duplicate element
 
 
+   Set<Integer> set = new HashSet<>();
 
-        System.out.println(adq.poll());
-        System.out.println(adq.pollFirst());
-        System.out.println(adq.pollLast());
+   set.add(30); 
+   set.add(40);  
+   set.add(50);  
+   set.add(60);  
+   set.add(30);  
 
-        System.out.println(adq);
+//  System.out.println(set);
+
+ set.remove(30);  
+ System.out.println(set.contains(30));
+ 
+ System.out.println(set);
 
     }
 }
