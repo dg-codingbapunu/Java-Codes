eb6baf464c04bdc869ac80736b3615934978c95f
patitapaban <patitapabanpanda046@gmail.com>
2024-11-12 11:41:01 +0530

    PriorityQueue min heap

--- hello.java
+++ hello.java
@@ -1,24 +1,23 @@
 import java.util.LinkedList;
+import java.util.PriorityQueue;
 import java.util.Queue;
 
 public class Hello {
 
     public static void main(String[] args) {
         
-   
-        Queue <Integer> queue = new LinkedList<>();
+    PriorityQueue<Integer> pq = new PriorityQueue<>();
 
+   pq.add(39);
+   pq.add(20);
+   pq.add(123);
+   pq.add(3);
+     
 
-        queue.offer(12);
-        queue.offer(122);
-        queue.offer(123);
-        queue.offer(132);
-
-        System.out.println("queue:" + queue);
-        System.out.println(queue.poll());
-        System.out.println(queue.peek());
-       
-
-
+pq.poll();
+System.out.println(pq);
+System.out.println(pq.peek());
+System.out.println(pq.poll());
+System.out.println(pq);
     }
 }
