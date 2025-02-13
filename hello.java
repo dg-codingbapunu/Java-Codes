c3bd42cc715d2eb3cc3bb4818fd32328e168c4bd
patitapaban <patitapabanpanda046@gmail.com>
2025-02-13 20:35:10 +0530

    methods

--- hello.java
+++ hello.java
@@ -8,13 +8,31 @@ public class Hello {
         p1.age = 20;
         p1.name = "Bapun";
 
+        Person p2 = new Person();
+        p2.name = "Sinu";
+        p2.age = 23;
+
+
         System.out.println(p1.name + "  " + p1.age);
 
+        System.out.println(p2.name + "  " + p2.age);
 
+        p1.walk();
+        p2.eat();
+        p1.eat();
+        p2.walk();
     }
 }
 
 class Person {
     String name;
     int age;
+
+    void walk() {
+        System.out.println(name +" is Walking ");
+
+    }
+    void eat() {
+        System.out.println(name + " is Eating ");
+    }
 }
