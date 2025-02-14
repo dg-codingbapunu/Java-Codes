4a67b82ea819b22c572d4b17e6d4237d05e5d8ff
patitapaban <patitapabanpanda046@gmail.com>
2025-02-14 19:03:12 +0530

    E

---
+++ encpse/Encapsulation.java
@@ -0,0 +1,14 @@
+package encpse;
+
+public class Encapsulation {
+  public static void main(String[] args) {
+    
+  }
+
+ public void doWork() {
+    System.out.println("Do your Work now");
+  }
+  
+
+}
+
--- hello.java
+++ hello.java
@@ -1,63 +1,21 @@
+import encpse.Encapsulation;
 
 public class Hello {
 
     public static void main(String[] args) {
         
 
-        Person p1 = new Person();
-        p1.age = 20;
-        p1.name = "Bapun";
 
-       
-        
-    Developer d1 = new Developer();
-    d1.age = 32;
-    d1.name = "Sagar";
+Encapsulation obj = new Encapsulation();
+obj.doWork();
 
-        d1.eat();
-       
 
-        System.out.println(p1.name + "  " + p1.age);
 
-       
-       
-        System.out.println(d1.name + "  " + d1.age);
-    
-        p1.eat();
-        p1.walk();
-       d1.walk();
+
+   
      
     }
 }
-
-class Developer extends Person{
-
-
-    void walk() {
-        System.out.println(" Developer " + name +" is Walking ");
-
-    }
-
-}
-
-class Person {
-    String name;
-    int age;
-   
-
-    
-    void walk() {
-        System.out.println(name +" is Walking ");
-
-    }
-
-    void eat() {
-        System.out.println(name + " is Eating ");
-    }
-
-   
-}
-
 
 
 
